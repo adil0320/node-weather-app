8a01f41d5c796b264da6f0b5d7c42a86fcfed9ff
adil0320 <adil.l@crestinfosystems.net>
2022-02-15 11:39:00 +0530

    Added more text to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,7 @@ const forecast = (longitude, latitude, callback) => {
     } else {
       callback(
         undefined,
-        `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degress out.`
+        `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degress out. The humidiity is ${body.current.humidity}%.`
       );
     }
   });
