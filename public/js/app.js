f8025c90fb3219e89a49351c593cef0cefa19a5a
adil0320 <adil.l@crestinfosystems.net>
2022-02-15 10:20:10 +0530

    initial commit

---
+++ public/js/app.js
@@ -0,0 +1,26 @@
+console.log('Client side javascript file is loaded');
+
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
+
+const fetchWeather = async address => {
+  messageOne.textContent = 'Loading...';
+  messageTwo.textContent = '';
+  const response = await fetch(
+    `http://localhost:3000/weather?address=${address}`
+  );
+  const responseData = await response.json();
+  if (responseData.error) {
+    return (messageOne.textContent = responseData.error);
+  }
+  messageOne.textContent = responseData.location;
+  messageTwo.textContent = responseData.forecast;
+};
+
+document.querySelector('form').addEventListener('submit', e => {
+  e.preventDefault();
+  const search = document.querySelector('input').value;
+  fetchWeather(search);
+});
+
+// fetchWeather();
---
+++ src/utils/forecast.js
@@ -0,0 +1,20 @@
+const request = require('request');
+
+const forecast = (longitude, latitude, callback) => {
+  const url = `http://api.weatherstack.com/current?access_key=8ded3bf5c1bcd1ab55732280279dc9ca&query=${latitude},${longitude}`;
+
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback('Unable to connect to weather service!', undefined);
+    } else if (error) {
+      callback('Unable to find location!', undefined);
+    } else {
+      callback(
+        undefined,
+        `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degress out.`
+      );
+    }
+  });
+};
+
+module.exports = forecast;
